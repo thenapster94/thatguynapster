f8ec834be597ee81732b5adedefffd619f9d2ad4
Napster <andrewosei94@gmail.com>
2021-06-26 09:34:52 +0000

    meta fixes

--- components/meta.jsx
+++ components/meta.jsx
@@ -11,9 +11,9 @@ export default function Meta({ canonical }) {
             <Head>
                 <meta charSet="UTF-8" key="charset" />
                 <meta name="viewport" content="width=device-width,initial-scale=1" key="viewport" />
-                <link rel="apple-touch-icon" href={`${process.env.NEXT_PUBLIC_BASE_URL}apple-touch-icon.png`} key="apple" />
-                <link rel="icon" type="image/png" sizes="32x32" href={`${process.env.NEXT_PUBLIC_BASE_URL}favicon-32x32.ico`} key="icon32" />
-                <link rel="icon" type="image/png" sizes="16x16" href={`${process.env.NEXT_PUBLIC_BASE_URL}favicon-16x16.ico`} key="icon16" />
+                <link rel="apple-touch-icon" href={`/apple-touch-icon.png`} key="apple" />
+                <link rel="icon" type="image/png" sizes="32x32" href={`/favicon-32x32.ico`} key="icon32" />
+                <link rel="icon" type="image/png" sizes="16x16" href={`/favicon-16x16.ico`} key="icon16" />
                 <link rel="icon" type="image/png" sizes="128x128" href={`/favicon.ico`} key="favicon" />
 
 
