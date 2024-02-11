cb508992535ecfa85b8083e5e0bca3aaf01c997d
thatGuyNapster <andrewosei94@gmail.com>
2024-02-11 20:33:41 +0000

    purge previous project files

--- configs/global_variables.jsx
+++
@@ -1,9 +0,0 @@
-export {};
-
-export const config = {
-  site_name: "ThatGuyNapster",
-  title: "Andrew Osei-Wusu | @thatguynapster",
-  description: "Portfolio website for Andrew Osei-Wusu (@thatguynapster)",
-  locale: "en",
-  canonical: "",
-};
