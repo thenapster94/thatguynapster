a8532bfc64bd2c8b4b599e551344ad02b4ff83c8
thatGuyNapster <andrewosei94@gmail.com>
2024-02-22 10:53:20 +0000

    default text color to white

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <body
-        className={` bg-[url('/img/page-background.webp')]  bg-top sm:bg-top bg-no-repeat`}
+        className={`text-white bg-[url('/img/page-background.webp')]  bg-top sm:bg-top bg-no-repeat`}
       >
         <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
         {children}
